116628971dfdbb556298d8fb19544f68811d047a
MrR736 <mrr6451@outlook.com>
2024-10-09 15:49:58 -0400

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -2,7 +2,7 @@
 
 case "$1" in
     gm)
-        wget -q -O"$SCRIPT_PACKAGE_TOOL/Downloads/GodotManager.zip" "https://github.com/MrR736/GodotManager/raw/refs/heads/main/Package/GodotManager.zip"
+        wget -q -O"/usr/scripts/ScriptPackageTool/Downloads/GodotManager.zip" "https://github.com/MrR736/GodotManager/raw/refs/heads/main/Package/GodotManager.zip"
         ;;
     *)
         echo "E: Unable to locate package $1"
