225ba21735fc9a4eef59d409f38cfee5dcd0b95e
MrR736 <mrr6451@outlook.com>
2024-10-10 16:00:34 -0400

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -6,6 +6,10 @@ echo "[https://github.com/MrR736]"
 case "$1" in
     gm)
         wget -q -O"$SCRIPTS_DIR/Downloads/Setup.zip" "https://github.com/MrR736/GodotManager/raw/refs/heads/main/Package/GodotManager.zip"
-        wget -q -O"$SCRIPTS_DIR/InstallLists/$1" "https://raw.githubusercontent.com/MrR736/GodotManager/refs/heads/main/Package/Releases"
+        GM_LATEST_VERSION=$(curl https://raw.githubusercontent.com/MrR736/GodotManager/refs/heads/main/Package/Releases)
+        cat > "$SCRIPTS_DIR/InstallLists/$1" <<EOL
+        GM_VERSION=$GM_LATEST_VERSION
+        GM_LATEST_VERSION=$(curl https://raw.githubusercontent.com/MrR736/GodotManager/refs/heads/main/Package/Releases)
+        EOL
         ;;
 esac
