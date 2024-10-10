ff426af2b7ea02bee26306f44a9c8846bef1ffaa
MrR736 <mrr6451@outlook.com>
2024-10-10 12:02:37 -0400

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
+echo "[https://github.com/MrR736]"
+
 case "$1" in
     gm)
         wget -q -O"/usr/scripts/ScriptPackageTool/Downloads/GodotManager.zip" "https://github.com/MrR736/GodotManager/raw/refs/heads/main/Package/GodotManager.zip"
-        ;;
-    *)
-        echo "E: Unable to locate package $1"
         ;;
 esac
