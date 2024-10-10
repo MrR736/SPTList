d19a31f4a40b309f47461e2f5bf4ef5fff66824c
MrR736 <mrr6451@outlook.com>
2024-10-10 12:14:23 -0400

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "[https://github.com/MrR736]"
 
 case "$1" in
     gm)
-        wget -q -O"/usr/scripts/ScriptPackageTool/Downloads/GodotManager.zip" "https://github.com/MrR736/GodotManager/raw/refs/heads/main/Package/GodotManager.zip"
+        wget -q -O"$SCRIPTS_DIR/Downloads/GodotManager.zip" "https://github.com/MrR736/GodotManager/raw/refs/heads/main/Package/GodotManager.zip"
         ;;
 esac
