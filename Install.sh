bc24d6a16d94a507be2a23f7d25d308728bd3fc9
MrR736 <mrr6451@outlook.com>
2024-10-17 09:49:15 -0400

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -6,11 +6,25 @@ echo "[https://github.com/MrR736]"
 case "$1" in
     gm)
         wget -q -O"$SCRIPTS_DIR/Downloads/Setup.zip" "https://github.com/MrR736/GodotManager/raw/refs/heads/main/Package/GodotManager.zip"
+        rm -rf "$SCRIPTS_DIR/Remove/$1"
         rm -rf "$SCRIPTS_DIR/InstallLists/$1.list"
         cat > "$SCRIPTS_DIR/InstallLists/$1.list" <<EOL
 COMMAND_NAME=$1
 VERSION=$(curl -s https://raw.githubusercontent.com/MrR736/GodotManager/refs/heads/main/Package/Releases)
 LATEST_VERSION=curl -s https://raw.githubusercontent.com/MrR736/GodotManager/refs/heads/main/Package/Releases
 EOL
+        ;;
+    rt)
+        wget -q -O"$SCRIPTS_DIR/Downloads/Setup.zip" "https://github.com/MrR736/RootTools/raw/refs/heads/main/Package/RootTools.zip"
+        rm -rf "$SCRIPTS_DIR/Remove/$1" "$SCRIPTS_DIR/InstallLists/$1.list"
+        cat > "$SCRIPTS_DIR/InstallLists/$1.list" <<EOL
+COMMAND_NAME=$1
+VERSION=$(curl -s https://raw.githubusercontent.com/MrR736/RootTools/refs/heads/main/Package/Releases)
+LATEST_VERSION=curl -s https://raw.githubusercontent.com/MrR736/RootTools/refs/heads/main/Package/Releases
+EOL
+        ;;
+    *)
+        echo "E: Unable To Locate Package $1"
+        exit 1
         ;;
 esac
